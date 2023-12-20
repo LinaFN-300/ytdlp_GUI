5ee79d7043bda307a8742543734e9d0638fc75f2
LinaFN-300 <iori.kdm.0513@icloud.com>
2023-12-21 01:09:34 +0900

    update

--- main.py
+++ main.py
@@ -3,18 +3,9 @@ import json
 from threading import Thread
 
 import tkinter as tk
+from tkinter import filedialog
 
 from yt_dlp import YoutubeDL
-import ffmpeg
-
-
-def my_hook(d):
-    if d['status'] == 'downloading':
-        # ETAが存在する場合のみ表示
-        if 'eta' in d:
-            statusbar.insert(f"Downloading: {d['_percent_str']} and ETA: {d['eta']} seconds")
-            #win.update_idletasks()
-            print(f"Downloading: {d['_percent_str']} and ETA: {d['eta']} seconds")
             
 #関数定義
 #ダウンロード関数
@@ -28,16 +19,24 @@ def download(url):
         except:
             dl_error()
 
+#進捗情報を処理するフック関数
+def progress_hooks(d):
+    if d['status'] == 'downloading':
+        #if 'eta' in d:
+            logbox.insert(tk.END, f"Downloading {d['tmpfilename']} ETA: {d['eta']}"+" seconds\n")
+            logbox.see(tk.END)
+
 #設定の読み込み
 def config_read():
     config_open = open('config.json', 'r')
     ydl_opts = json.load(config_open)
-    ydl_opts = dict(ydl_opts, progress_hooks=[my_hook])
+    ydl_opts = dict(ydl_opts, progress_hooks=[progress_hooks])
     print(ydl_opts)
     return ydl_opts
 
 #設定の書き込み
 def config_write(conf):
+    conf = dict(conf, writethumbnail="True")
     config_open = open('config.json', 'w')
     json.dump(conf, config_open, indent=4)
 
@@ -65,11 +64,108 @@ def start_GUI():
 
     #ステータスバー
     global statusbar
-    statusbar = tk.Entry(width=20)
+    statusbar = tk.Entry(width=13)
     statusbar.insert(tk.END, "Stundby!")
     statusbar.place(x=45, y=45)
 
+    #進捗
+    global progress_info, logbox
+    logbox = tk.Text(width=60, height=10)
+    logbox.bind("<Key>", lambda a: "break")
+    logbox.place(x=45, y=70)
+
+    #設定画面を開くボタン
+    ConfigButton = tk.Button(text=u'Config', width=10)
+    ConfigButton.bind("<Button-1>", lambda event: config_window())
+    ConfigButton.place(x=415, y=42)
+
     win.mainloop()
+
+#設定画面
+def config_window():
+    #windowの作成
+    global config_win
+    config_win = tk.Toplevel(win)
+    config_win.title(u"Config Menu")
+    config_win.geometry("500x450")
+
+    #保存先のpath指定
+    #ラベル
+    label1 = tk.Label(config_win, text=u'ダウンロード先->')
+    label1.place(x=5, y=20)
+    #path入力欄
+    InputPathBox = tk.Entry(config_win, width=60)
+    InputPathBox.place(x=85, y=20)
+    #フォルダー選択ボタン
+    PathButton = tk.Button(config_win, text=u'参照', width=3)
+    PathButton.place(x=420, y=17)
+    PathButton.bind("<Button-1>", lambda event: InputPathBox.insert(tk.END, filedialog.askdirectory()))
+    
+    #音声だけダウンロードするかどうか
+    #ラベル
+    label2 = tk.Label(config_win, text=u'音声のみダウンロード')
+    label2.place(x=40, y=45)
+    #チェックボックス
+    AudioOnly = tk.BooleanVar()
+    AudioOnly.set(False)
+    CheckButton = tk.Checkbutton(config_win, variable=AudioOnly)
+    CheckButton.place(x=10, y=45)
+    
+    #ブラウザからcookieを取得するかどうか
+    #ラベル
+    label3 = tk.Label(config_win, text=u'ブラウザからcookieを取得：ブラウザ名->')
+    label3.place(x=40, y=70)
+    #チェックボックス
+    CookieFromBrowser = tk.BooleanVar()
+    CookieFromBrowser.set(False)
+    CheckButton = tk.Checkbutton(config_win, variable=CookieFromBrowser)
+    CheckButton.place(x=10, y=70)
+    #ブラウザ名入力欄
+    InputBrowserBox = tk.Entry(config_win, width=10)
+    InputBrowserBox.place(x=240, y=70)
+    
+    #cookieのpath指定
+    #ラベル
+    label4 = tk.Label(config_win, text=u'cookieをファイルから取得')
+    label4.place(x=40, y=95)
+    #チェックボックス
+    Cookie = tk.BooleanVar()
+    Cookie.set(False)
+    CheckButton = tk.Checkbutton(config_win, variable=Cookie)
+    CheckButton.place(x=10, y=95)
+    #ラベル
+    label5 = tk.Label(config_win, text=u'cookies.txt->')
+    label5.place(x=5, y=120)
+    #path入力欄
+    InputCookieBox = tk.Entry(config_win, width=60)
+    InputCookieBox.place(x=85, y=120)
+    #ファイル選択ボタン
+    CookieButton = tk.Button(config_win, text=u'参照', width=3)
+    CookieButton.place(x=420, y=117)
+    CookieButton.bind("<Button-1>", lambda event: InputCookieBox.insert(tk.END, filedialog.askopenfilename()))
+    
+    #設定を保存するボタン
+    SaveButton = tk.Button(config_win, text=u'保存', width=10)
+    SaveButton.place(x=415, y=415)
+    SaveButton.bind("<Button-1>", lambda event: SaveConfig())
+    
+    #設定を保存
+    def SaveConfig():
+        conf = {}
+        conf = dict(conf, outtmpl=InputPathBox.get()+"/%(title)s.%(ext)s")
+        if AudioOnly.get() == True:
+            conf = dict(conf, format="bestaudio")
+            conf = dict(conf, postprocessors=[{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3', 'preferredquality': '192'}])
+        else:
+            conf = dict(conf, format="bestvideo+bestaudio/best")
+        if CookieFromBrowser.get() == True:
+            conf = dict(conf, cookieflombrowser=InputBrowserBox.get())
+        if Cookie.get() == True:
+            conf = dict(conf, cookiefile=InputCookieBox.get())
+        config_write(conf)
+            
+    
+    config_win.mainloop()
 
 #ステータスバーの更新
 #ダウンロード開始時の処理
@@ -81,6 +177,7 @@ def dl_start():
 def dl_success():
     statusbar.delete(0, tk.END)
     statusbar.insert(tk.END, "Success!")
+    logbox.insert(tk.END, "Done.\n\n")
 
 #ダウンロード失敗時の処理
 def dl_error():
